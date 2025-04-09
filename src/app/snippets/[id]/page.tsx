cdd9c082fd4d1d29f7b9dd55b00ed70f3053cea4
Georgi Georgiev <georgigeorgiev@iMac-von-Georgi.fritz.box>
2025-04-09 09:00:29 +0200

    added caching

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { db } from '@/db'
+import * as actions from '@/actions'
 
 interface SnippetShowPageProps {
   params: Promise<{
@@ -18,15 +19,22 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     return notFound()
   }
 
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id)
+
   return (
     <div>
       <div className='flex m-4 justify-between items-center'>
         <h1 className='text-xl font-bold'>{snippet.title}</h1>
         <div className='flex gap-4'>
-          <Link href={`/snippets/${snippet.id}`} className='p-2 border rounded'>
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className='p-2 border rounded'
+          >
             Edit
           </Link>
-          <button className='p-2 border rounded'>Delete</button>
+          <form action={deleteSnippetAction}>
+            <button className='p-2 border rounded'>Delete</button>
+          </form>
         </div>
       </div>
       <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
@@ -34,4 +42,14 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
       </pre>
     </div>
   )
+}
+
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany()
+
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString(),
+    }
+  })
 }
